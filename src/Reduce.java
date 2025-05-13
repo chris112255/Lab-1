4b0b9e9e4cccf2352cc197168cb9dc8ce39f92df
Chris <chris.zheng@mail.utoronto.ca>
2025-05-12 20:34:55 -0400

    added multiples.java

---
+++ src/Reduce.java
@@ -0,0 +1,21 @@
+public class Reduce {
+    public static void main(String[] args) {
+        // starting with a number n, if n is even divide it by 2. If n is odd, subtract 1. Repeat.
+
+        int start = 100;
+        int steps = 0;
+
+        while(start > 0){
+            if(start % 2 == 0){
+                start = start / 2;
+            }
+            else{
+                start -= 1;
+            }
+            steps += 1;
+            System.out.println(start);
+        }
+
+        System.out.println(steps);
+    }
+}
